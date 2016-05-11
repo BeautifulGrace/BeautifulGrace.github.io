3461fc1d67b64375f13b01fee9b6cdd33db42255
BeautifulGrace <1136067972@qq.com>
2016-05-11 12:51:45 +0800

    -m

---
+++ 11.4/hello.js
@@ -0,0 +1,16 @@
+var age = 90;
+if (age > 40) {
+	if (age >= 90){ alert("耄耋之年");} else {
+		alert("四十不惑");
+	}
+} else {
+	alert("人生的路还很长");
+}
+
+for (var i = 0; i < 100; i++) {
+	console.log(i)
+}
+
+function sayHello(name){
+	console.log(name + ",你好");
+}
